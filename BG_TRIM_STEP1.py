79403f37147b421c6383aeab862d64f985d1202e
Daniel Fux <daniel.fux@intel.com>
2022-11-10 18:56:25 +0200

    Have some stuff need to finish

--- BG_TRIM_STEP1.py
+++ BG_TRIM_STEP1.py
@@ -5,6 +5,9 @@
 
 # Constants #
 ListDTS = ['dts0_aon', 'dts1', 'dts2', 'dts3', 'dts_ccf0' , 'dts_ccf1', 'dts_gt0', 'dts_gt1']
+VinADC = 0.77
+VrefADC = 0.93
+MeasurementsNum = 100
 
 if __name__ == '__main__':
     print('The function will get BG TRIM - STEP 1 on of the following DTSs:')
@@ -22,34 +25,50 @@ if __name__ == '__main__':
             break
 
     # a. program 1.2V LDO reference selection mux to take lvr ref as reference voltage
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.ldo1p2_ext_vref_sel = 0x0'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.ldo1p2_ext_vref_sel = 0x0'
     exec(command)
 
     # b. Program 1.2V LDO resistance divider correspondingly to take lvr ref of 0.93V as input reference voltage
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg7_inst.ldo1p2_vref_range_sel = 110'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg7_inst.ldo1p2_vref_range_sel = 110'
     exec(command)
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.ldo1p2_out_sel = 10'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.ldo1p2_out_sel = 10'
     exec(command)
 
     # c. Lvr ref_en to be asserted post vccehv is applied & stable
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg5_inst.lvrref_en = 0x1'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg5_inst.lvrref_en = 0x1'
     exec(command)
 
     # d. Program ADC external reference mux to select lvr ref
-    command = 'taps.cdie_' + name + '.tapconfig.adcdfxextvref = 0x0'
+    command = 'cdie.taps.cdie_' + name + '.tapconfig.adcdfxextvref = 0x0'
     exec(command)
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg4_inst.adc_vrefldo_ext_vref_sel = 1111'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg4_inst.adc_vrefldo_ext_vref_sel = 1111'
     exec(command)
 
     # e. Program vref_ldo output mux to select external reference as adc reference voltage
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg4_inst.adc_vrefldo_ext_vref_sel = 0x1'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg4_inst.adc_vrefldo_ext_vref_sel = 0x1'
     exec(command)
 
     # f. Program ADC supply buffer to select external input reference voltage
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg16_inst.adc_supply_buf_vref_ext_sel = 0x1'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg16_inst.adc_supply_buf_vref_ext_sel = 0x1'
     exec(command)
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.adc_supply_buf_out_sel = 0x1'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.adc_supply_buf_out_sel = 0x1'
     exec(command)
+
+    # 2. config ADC
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg14_inst.adcvinsel0 = 11'
+    exec(command)
+    command = 'cdie.taps.cdie_' + name '.tapconfig.anadfxinen = 11'
+    exec(command)
+
+    command = ''  # TBD - check wat is the command for getting bgrtirmcode
+    SumBGRCode = 0
+    # TBD - how to configure to 10 bits code?
+
+    for i in range(MeasurementsNum):
+        SumBGRCode += eval(command)
+
+    AverageCode = SumBGRCode / MeasurementsNum  # In what register save the resualt? TBD
+
 
 
 
--- BG_TRIM_STEP2.py
+++ BG_TRIM_STEP2.py
@@ -5,6 +5,9 @@
 
 # Constants #
 ListDTS = ['dts0_aon', 'dts1', 'dts2', 'dts3', 'dts_ccf0', 'dts_ccf1', 'dts_gt0', 'dts_gt1']
+VinADC = 0.77
+VrefADC = 0.93
+MeasurementsNum = 100
 
 if __name__ == '__main__':
     print('The function will get BG TRIM - STEP 2 on of the following DTSs:')
@@ -22,33 +25,76 @@ if __name__ == '__main__':
             break
 
     # a. program 1.2V LDO reference selection mux to take lvr ref as reference voltage
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.ldo1p2_ext_vref_sel = 0x0'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.ldo1p2_ext_vref_sel = 0x0'
     exec(command)
 
     # b. Program 1.2V LDO resistance divider correspondingly to take lvr ref of 0.8V as input reference voltage
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg7_inst.ldo1p2_vref_range_sel = 100'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg7_inst.ldo1p2_vref_range_sel = 100'
     exec(command)
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.ldo1p2_out_sel = 10'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.ldo1p2_out_sel = 10'
     exec(command)
 
     # c. Program vref_ldo to take internal BG reference as input
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg4_inst.adc_vrefldo_ext_vref_sel = 0x0'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg4_inst.adc_vrefldo_ext_vref_sel = 0x0'
     exec(command)
 
     # d. Program vref_ldo resistance divider to take 0.7V as input reference voltage – functional scenario
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg6_inst.vrefldo_vref_range_sel = 10'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg6_inst.vrefldo_vref_range_sel = 10'
     exec(command)
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.adc_vrefldo_out_sel = 0x1'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg32_inst.adc_vrefldo_out_sel = 0x1'
     exec(command)
 
     # e. Lvr ref_en to be asserted post vccehv is applied & stable
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg5_inst.lvrref_en = 0x1'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg5_inst.lvrref_en = 0x1'
     exec(command)
 
     # Take Vref_ldo output as reference voltage for ADC & Supply_buffer which is the case of regular
     # functional mode operation
-    command = 'soc_cr_wrapper.' + name + '.inst0.dfvfreg15_inst.adcvrefbufsel = 0x1'
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg15_inst.adcvrefbufsel = 0x1'
     exec(command)
+
+    # config ADC - Value must be same as chosen in Step 1 procedure
+    command = 'cdie.soc_cr_wrapper.' + name + '.inst0.dfvfreg14_inst.adcvinsel0 = 11'
+    exec(command)
+    command = 'cdie.taps.cdie_' + name '.tapconfig.anadfxinen = 11'
+    exec(command)
+
+    SumBGRCode = 0
+    AvgStep1Code = 0  #### TBD - insert the code from the register
+    command = '# TBD - how to configure to 10 bits code?'
+    exec(command)
+
+    MeasureBGRCommand = ''  # TBD - check what is the command for getting bgrtirmcode
+    IncrementBgrtrim = ''  # TBD - check what is the command for increment in one step bgrtirmcode
+    DecrementBgrtrim = ''  # TBD - check what is the command for decrement in one step bgrtirmcode
+
+    # First, we will check if we need to increase or to decrease the bgtrimcode in order to get to the right val
+    for i in range(MeasurementsNum):
+        SumBGRCode += eval(MeasureBGRCommand)
+
+    AverageCode = SumBGRCode / MeasurementsNum
+
+    if AverageCode > AvgStep1Code:
+        while AverageCode > AvgStep1Code:
+            exec(DecrementBgrtrim)
+            # time.sleep(1) - TBD check what is right command
+            for i in range(MeasurementsNum):
+                SumBGRCode += eval(MeasureBGRCommand)
+
+            AverageCode = SumBGRCode / MeasurementsNum
+        exec(IncrementBgrtrim)
+        # time.sleep(1) - TBD check what is right command
+
+    else:
+        while AverageCode < AvgStep1Code:
+            exec(IncrementBgrtrim)
+            # time.sleep(1) - TBD check what is right command
+            for i in range(MeasurementsNum):
+                SumBGRCode += eval(MeasureBGRCommand)
+
+            AverageCode = SumBGRCode / MeasurementsNum
+
+
 
 
 
